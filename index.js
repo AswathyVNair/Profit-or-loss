64d499789cb0e9e734d42a4d93d82ddb60808648
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-10-02 16:45:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-button");
+var outputBox = document.querySelector("#output-box");
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    showOutput(
+      `Hey the loss is ${loss} and the percentage is ${lossPercentage}%`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    showOutput(
+      `Hey the profit is ${profit} and the percentage is ${profitPercentage}%`
+    );
+  } else {
+    showOutput("No pain no gain and no gain no pain");
+  }
+}
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stockQuantity.value);
+  var curr = Number(currentPrice.value);
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+function showOutput(message) {
+  outputBox.innerText = message;
+}
+
+submitBtn.addEventListener("click", submitHandler);
